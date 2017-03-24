f5d304c32e8eeccc575b9e7740625e0ed4d036f3
Luke Li <leezhc@gmail.com>
2017-03-24 00:52:04 -0700

    todo first version finished

--- hello-angular2/src/app/todo/todo.component.ts
+++ hello-angular2/src/app/todo/todo.component.ts
@@ -10,15 +10,47 @@ import { TodoService } from './todo.service'
 })
 export class TodoComponent implements OnInit {
   todos: Todo[] = [];
-  desc = "";
+  desc: string = "";
 
   constructor(private service: TodoService) { }
 
   ngOnInit() {
+    this.getTodos();
   }
   addTodo(){
-    this.todos = this.service.addTodo(this.desc);
-    this.desc = "";
+    this.service.addTodo(this.desc)
+    .then(todo => {
+      this.todos = [... this.todos, todo];
+      this.desc = "";
+    }); 
   }
 
+    toggleTodo(todo: Todo) {
+    const i = this.todos.indexOf(todo);
+    this.service
+      .toggleTodo(todo)
+      .then(t => {
+        this.todos = [
+          ...this.todos.slice(0,i),
+          t,
+          ...this.todos.slice(i+1)
+          ];
+      });
+  }
+  removeTodo(todo: Todo) {
+    const i = this.todos.indexOf(todo);
+    this.service
+      .deleteTodoById(todo.id)
+      .then(()=> {
+        this.todos = [
+          ...this.todos.slice(0,i),
+          ...this.todos.slice(i+1)
+        ];
+      });
+  }
+  getTodos(): void {
+    this.service
+      .getTodos()
+      .then(todos => this.todos = [...todos]);
+  }
 }
--- hello-angular2/src/app/todo/todo.service.ts
+++ hello-angular2/src/app/todo/todo.service.ts
@@ -1,25 +1,68 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
-import { Todo } from './todo.model';
+
 import { UUID } from 'angular2-uuid';
 
 import 'rxjs/add/operator/toPromise';
-
+import { Todo } from './todo.model';
 @Injectable()
 export class TodoService {
 
-  todos: Todo[] = [];
+// WebApi 
+private api_url = 'api/todos';
+private headers = new Headers ({'Content-Type':'application/json'});
 
-  constructor() { }
-  addTodo(todoItem:string): Todo[]{
+//  todos: Todo[] = [];
+
+  constructor(private http: Http) { }
+
+  // POST todos
+  addTodo(desc:string): Promise<Todo> {
    // let iid = parseFloat(UUID.UUID()) ;
     let todo = {
    //   id: iid,
       id: UUID.UUID(),
-      desc: todoItem,
+      desc: desc,
       completed: false
-    }
-    this.todos.push(todo);
-    return this.todos;
+    };
+    //this.todos.push(todo);
+    //return this.todos;
+    return this.http
+            .post(this.api_url, JSON.stringify(todo), {headers: this.headers})
+            .toPromise()
+            .then(res => res.json().data as Todo)
+            .catch(this.handleError);
+  }
+    // PUT /todos/:id
+  toggleTodo(todo: Todo): Promise<Todo> {
+    const url = `${this.api_url}/${todo.id}`;
+    console.log(url);
+    let updatedTodo = Object.assign({}, todo, {completed: !todo.completed});
+    return this.http
+            .put(url, JSON.stringify(updatedTodo), {headers: this.headers})
+            .toPromise()
+            .then(() => updatedTodo)
+            .catch(this.handleError);
+  }
+  // DELETE /todos/:id
+  deleteTodoById(id: string): Promise<void> {
+    const url = `${this.api_url}/${id}`;
+    return this.http
+            .delete(url, {headers: this.headers})
+            .toPromise()
+            .then(() => null)
+            .catch(this.handleError);
+  }
+  // GET /todos
+  getTodos(): Promise<Todo[]>{
+    return this.http.get(this.api_url)
+              .toPromise()
+              .then(res => res.json().data as Todo[])
+              .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any>{
+    console.error('ERROR: ', error);
+    return Promise.reject(error.message || error);
   }
 }

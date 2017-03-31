157ab601c3d6a7d18682dfd3a4a2481e7e3e185d
Luke Li <leezhc@gmail.com>
2017-03-30 22:45:09 -0700

    initial for trial-app

---
+++ trial-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { FrontPageComponent } from './front-page/front-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FrontPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
